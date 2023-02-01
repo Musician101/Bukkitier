ed0f2d39ff7c7e4047629f30248c6ecccf02daa6
Musician101 <bruceabivens@gmail.com>
2023-01-31 23:54:17 -0800

    Remove commented out dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,19 +22,13 @@ repositories {
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") {
         name = "SpigotMC"
     }
-    /*maven("https://papermc.io/repo/repository/maven-public/") {
-        name = "papermc-repo"
-    }*/
     mavenLocal()
 }
 
 dependencies {
     compileOnly("com.google.code.findbugs:jsr305:3.0.2")
     compileOnlyApi("org.spigotmc:spigot-api:1.19.3-R0.1-SNAPSHOT")
-    //TODO temp
-    //compileOnlyApi("org.spigotmc:spigot:1.19.3-R0.1-SNAPSHOT")
     compileOnlyApi("com.mojang:brigadier:1.0.18")
-    //compileOnlyApi("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
 }
 
 publishing {
